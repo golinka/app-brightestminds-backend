5c85faafde8cc8b0cb19a6b7762e875f67504a37
golinka <artem.holinka@gmail.com>
2019-01-28 23:05:11 +0200

    feat(models): added role model and edited user model

--- app/Models/User.js
+++ app/Models/User.js
@@ -19,6 +19,14 @@ class User extends Model {
     });
   }
 
+  static get traits() {
+    return ["@provider:Adonis/Acl/HasRole"];
+  }
+
+  static get hidden() {
+    return ["password"];
+  }
+
   /**
    * A relationship on tokens is required for auth to
    * work. Since features like `refreshTokens` or
