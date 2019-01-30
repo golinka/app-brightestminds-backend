24d12b225e48e0cfaf4496ae81775ad178697ef6
golinka <artem.holinka@gmail.com>
2019-01-30 16:01:14 +0200

    feat(routes): init

--- start/routes.js
+++ start/routes.js
@@ -14,6 +14,33 @@
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use("Route");
 
-Route.get("/", () => {
-  return { greeting: "Hello world in JSON" };
-});
+Route.group(() => {
+  Route.post("/login", "AuthController.login");
+  Route.get("/logout", "AuthController.logout");
+
+  Route.get("/products", "ProductController.index");
+  Route.get("/products/:pid", "ProductController.show");
+})
+  .prefix("api/v1")
+  .middleware("guest");
+
+Route.group(() => {
+  Route.get("/products/all", "ProductController.indexAll");
+  Route.post("/products", "ProductController.store");
+  Route.post("/products/:pid", "ProductController.update");
+  Route.delete("/products/:pid", "ProductController.delete");
+
+  Route.get("/users", "UserController.index");
+  Route.post("/users", "UserController.store");
+  Route.get("/users/:uid", "UserController.show");
+  Route.post("/users/:uid", "UserController.update");
+  Route.delete("/users/:uid", "UserController.delete");
+
+  Route.get("/services", "UserController.index");
+  Route.post("/services", "UserController.store");
+  Route.get("/services/:sid", "UserController.show");
+  Route.post("/services/:sid", "UserController.update");
+  Route.delete("/services/:sid", "UserController.delete");
+})
+  .prefix("api/v1")
+  .middleware(["auth", "is:admin"]);
