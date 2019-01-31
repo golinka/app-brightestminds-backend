da695c18848f7d10bd87042ca3ce1e4ec8f9c9ca
golinka <artem.holinka@gmail.com>
2019-01-31 11:27:38 +0200

    feat(controllers): init product controller

---
+++ app/Controllers/Http/ProductController.js
@@ -0,0 +1,40 @@
+const Product = use("App/Models/Product");
+
+class ProductController {
+  async index({ request }) {
+    const { all } = request.get();
+    return Product.index(all);
+  }
+
+  async show({ params }) {
+    const { pid } = params;
+    return Product.findOrFail(pid);
+  }
+
+  async store({ request }) {
+    return Product.create(
+      request.only(["title", "description", "price", "period", "is_private"])
+    );
+  }
+
+  async update({ request, params }) {
+    const { pid } = params;
+    const data = request.only([
+      "title",
+      "description",
+      "price",
+      "period",
+      "is_private"
+    ]);
+    return Product.update(pid, data);
+  }
+
+  async delete({ params, response }) {
+    const { pid } = params;
+    const product = await Product.findOrFail(pid);
+    await product.delete();
+    return response.status(204).send();
+  }
+}
+
+module.exports = ProductController;
