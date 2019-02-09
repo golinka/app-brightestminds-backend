a911b42baeb1b60707fc31b00fe040b1f45ba346
golinka <artem.holinka@gmail.com>
2019-02-09 21:55:58 +0200

    chore(routes): restructured

--- start/routes.js
+++ start/routes.js
@@ -11,6 +11,7 @@
 |
 */
 
+/* eslint-disable */
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use("Route");
 
@@ -22,37 +23,24 @@ Route.group(() => {
 
   Route.get("/products", "ProductController.index");
   Route.get("/products/:pid", "ProductController.show");
-  Route.get("/products/:pid/buy", "SubscriptionController.buy").middleware(
-    "auth"
-  );
+  Route.get("/products/:pid/buy", "SubscriptionController.buy").middleware("auth");
+  Route.post("/products", "ProductController.store").middleware(["auth", "is:admin"]);
+  Route.post("/products/:pid", "ProductController.update").middleware(["auth", "is:admin"]);
+  Route.delete("/products/:pid", "ProductController.delete").middleware(["auth", "is:admin"]);
 
+  Route.get("/users", "UserController.index").middleware(["auth", "is:admin"]);
   Route.post("/users", "UserController.store");
   Route.get("/users/:uid", "UserController.show").middleware("canSee");
   Route.post("/users/:uid", "UserController.update").middleware("canSee");
   Route.delete("/users/:uid", "UserController.delete").middleware("canSee");
-  Route.get("/users/:uid/subscriptions", "UserController.userSubs").middleware(
-    "canSee"
-  );
+  Route.get("/users/:uid/subscriptions", "UserController.userSubs").middleware("canSee");
 
-  Route.get("/subscriptions/:sid", "SubscriptionController.show").middleware(
-    "canSee"
-  );
+  Route.get("/subscriptions", "SubscriptionController.index").middleware(["auth", "is:admin"]);
+  Route.get("/subscriptions/:sid", "SubscriptionController.show").middleware("canSee");
+
+  Route.get("/services", "ServiceController.index").middleware(["auth", "is:admin"]);
+  Route.post("/services", "ServiceController.store").middleware(["auth", "is:admin"]);
+  Route.get("/services/:sid", "ServiceController.show").middleware(["auth", "is:admin"]);
+  Route.post("/services/:sid", "ServiceController.update").middleware(["auth", "is:admin"]);
+  Route.delete("/services/:sid", "ServiceController.delete").middleware(["auth", "is:admin"]);
 }).prefix("api/v1");
-
-Route.group(() => {
-  Route.post("/products", "ProductController.store");
-  Route.post("/products/:pid", "ProductController.update");
-  Route.delete("/products/:pid", "ProductController.delete");
-
-  Route.get("/users", "UserController.index");
-
-  Route.get("/services", "ServiceController.index");
-  Route.post("/services", "ServiceController.store");
-  Route.get("/services/:sid", "ServiceController.show");
-  Route.post("/services/:sid", "ServiceController.update");
-  Route.delete("/services/:sid", "ServiceController.delete");
-
-  Route.get("/subscriptions", "SubscriptionController.index");
-})
-  .prefix("api/v1")
-  .middleware(["auth", "is:admin"]);
