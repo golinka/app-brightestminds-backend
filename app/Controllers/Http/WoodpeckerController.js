84db0f44d94ccb5a151eb975e6d4bb094c444f3d
Artem Holinka <artem.holinka@gmail.com>
2019-02-20 18:45:46 +0200

    chore(controllers): refactoring

--- app/Controllers/Http/WoodpeckerController.js
+++ app/Controllers/Http/WoodpeckerController.js
@@ -1,26 +1,29 @@
 const Service = use("App/Models/Service");
 
 class WoodpeckerController {
-  async campaigns({ auth }) {
-    const user = await auth.getUser();
+  async getServiceToken(user) {
     const { rows: [woodpecker] } = await user.services().where("slug", "woodpecker").fetch(); // eslint-disable-line
     const { token } = woodpecker.toJSON().pivot;
+    return token;
+  }
+
+  async campaigns({ auth }) {
+    const user = await auth.getUser();
+    const token = await this.getServiceToken(user);
     return Service.getCampaigns(token);
   }
 
   async campaign({ params, auth }) {
     const { cid } = params;
     const user = await auth.getUser();
-    const { rows: [woodpecker] } = await user.services().where("slug", "woodpecker").fetch(); // eslint-disable-line
-    const { token } = woodpecker.toJSON().pivot;
+    const token = await this.getServiceToken(user);
     return Service.getCampaign(cid, token);
   }
 
   async prospects({ params, auth }) {
     const { cid } = params;
     const user = await auth.getUser();
-    const { rows: [woodpecker] } = await user.services().where("slug", "woodpecker").fetch(); // eslint-disable-line
-    const { token } = woodpecker.toJSON().pivot;
+    const token = await this.getServiceToken(user);
     return Service.getProspects(cid, token);
   }
 }
