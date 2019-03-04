2a338ad831c97b1b2d5799054e3303f3e6261da4
golinka <artem.holinka@gmail.com>
2019-03-04 22:25:53 +0200

    chore(controllers): added user info to auth response

--- app/Controllers/Http/AuthController.js
+++ app/Controllers/Http/AuthController.js
@@ -1,7 +1,14 @@
+const User = use("App/Models/User");
+
 class AuthController {
   async login({ request, auth }) {
     const { username, password } = request.all();
-    return auth.attempt(username, password);
+    const response = await auth.attempt(username, password);
+    if (typeof response === "object") {
+      const user = await User.findByOrFail({ username });
+      Object.assign(response, { user });
+    }
+    return response;
   }
 }
 
