e5efccf2846ebfd39dea2c2c5842f77f66679d14
golinka <artem.holinka@gmail.com>
2019-03-12 15:27:34 +0200

    chore(validators): removed unnecessary validation functions

--- app/Validators/CheckUser.js
+++ app/Validators/CheckUser.js
@@ -1,33 +1,8 @@
-const Validator = use("Validator");
-const User = use("App/Models/User");
-
-const checkFn = async (auth, field, value) => {
-  const user = await auth.getUser();
-  const { rows: users } = await User.all();
-  const isNotUnique = users.map(man => man[field] === value).includes(true);
-  return user[field] !== value && isNotUnique;
-};
-
-const usernameFn = async (data, field, message) => {
-  const { username, auth } = data;
-  const fails = await checkFn(auth, "username", username);
-  if (fails) throw message;
-};
-
-const useremailFn = async (data, field, message) => {
-  const { email, auth } = data;
-  const fails = await checkFn(auth, "email", email);
-  if (fails) throw message;
-};
-
-Validator.extend("username", usernameFn);
-Validator.extend("useremail", useremailFn);
-
 class CheckUser {
   get rules() {
     return {
-      username: "required|min:4|max:80|username",
-      email: "required|email|max:255|useremail",
+      username: "required|min:4|max:80|unique:users",
+      email: "required|email|max:255|unique:users",
       password: "required|min:4|max:12",
       fname: "required|min:2|max:35",
       lname: "min:2|max:45",
@@ -40,8 +15,8 @@ class CheckUser {
     return {
       required: "{{ field }} is required",
       email: "{{ field }} is in wrong format",
-      "username.username": "{{ field }} value already use",
-      "email.useremail": "{{ field }} value already use",
+      "username.unique": "{{ field }} value already use",
+      "email.unique": "{{ field }} value already use",
       "username.min": "{{ field }} must be at least 4 characters",
       "username.max": "{{ field }} must be at maximum 80 characters",
       "email.max": "{{ field }} must be at maximum 255 characters",
